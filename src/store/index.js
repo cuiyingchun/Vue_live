412239c279c4286f0f84bb1200ff266475bf8e02
cuiyingchun <3075382844@qq.com>
2018-05-06 11:39:47 +0800

    一个vue的直播demo

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2018/5/6.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+import state from './state'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+})
